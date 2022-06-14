fedd2bde179a9094a1d66c9553045d7d52b420ea
RobinLoksli92 <gleb11tiun@mail.ru>
2022-06-14 14:01:26 +0200

    Add frontend

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from .views import show_general
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', show_general)
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
