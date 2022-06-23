138e0dd50de5e297c580dce43e88b6ff7167972a
RobinLoksli92 <gleb11tiun@mail.ru>
2022-06-23 14:27:29 +0200

    Add new command for place loading

--- mysite/settings.py
+++ mysite/settings.py
@@ -117,10 +117,12 @@ STATIC_URL = '/static/'
 
 MEDIA_URL = '/media/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
-
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+if DEBUG:
+    STATICFILES_DIRS = [
+        os.path.join(BASE_DIR, "static"),
+    ]
+else:
+    STATIC_ROOT = os.path.join(BASE_DIR, "static")
+    
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
